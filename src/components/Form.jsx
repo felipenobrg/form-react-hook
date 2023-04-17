9afc1aeb8bdcc0904711b0caa835f09779f05f04
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-04-17 20:18:46 -0300

    finishing schema

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,8 +5,12 @@ export const Form = () => {
     const { register, handleSubmit } = useForm()
 
     const schema = yup.object().shape({
-        fullName: yup.string
-    })
+        fullName: yup.string().required(),
+        email: yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword: yup.string().oneOf([yup.ref("password"), null])
+})
 
     const onSubmit = () => {
         console.log("Hello World!")
@@ -18,7 +22,7 @@ export const Form = () => {
         <input type="text" placeholder="Full Name..." {...register("fullName")} />
         <input type="text" placeholder="Email..." {...register("email")} />
         <input type="number\" placeholder="Age..." {...register("age")} />
-        <input type="password" placeholder="Password..." {...register("password")} />
+        <input ype="password" placeholder="Password..." {...register("password")} />
         <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")} />
         <input type="submit" />
       </form>
