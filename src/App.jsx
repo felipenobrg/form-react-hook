4454044198ab26bd7893b57bf4a417340a349685
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-04-18 07:28:31 -0300

    adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Form } from "./components/Form"
+import './App.css'
 
 function App() {
   return (
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ import * as yup from 'yup'
 export const Form = () => {
     const schema = yup.object().shape({
         fullName: yup.string().required("Your Full Name is required"),
-        email: yup.string().email().required("Your Full Name is required"),
+        email: yup.string().email().required("Your Email is required"),
         age: yup.number().positive().integer().min(18).required("Your Age is required"),
         password: yup.string().min(4).max(20).required(),
         confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords Don't Match").required(),
@@ -25,21 +25,24 @@ export const Form = () => {
     }
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}> 
-        <input type="text" placeholder="Full Name..." {...register("fullName")} />
+    <div className='App' >
+      <div className="input-container">
+      <h2>Sign Up</h2>
+      <form className='form' onSubmit={handleSubmit(onSubmit)}> 
+        <input className='fullName' type="text" placeholder="Full Name..." {...register("fullName")} />
         <p>{errors.fullName?.message}</p>
-        <input type="text" placeholder="Email..." {...register("email")} />
+        <input className='email' type="email" placeholder="Email..." {...register("email")} />
         <p>{errors.email?.message}</p>
-        <input type="number\" placeholder="Age..." {...register("age")} />
-        <p>{errors.number?.message}</p>
-        <input ype="password" placeholder="Password..." {...register("password")} />
+        <input className='age' type="number" placeholder="Age..." {...register("age")} />
+        <p>{errors.age?.message}</p>
+        <input className='password' type="password" placeholder="Password..." {...register("password")} />
         <p>{errors.password?.message}</p>
-        <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")} />
+        <input className='confirmPassword' type="password" placeholder="Confirm Password..." {...register("confirmPassword")} />
         <p>{errors.confirmPassword?.message}</p>
-        <input type="submit" />
+        <input className='inputSubmit' type="submit" value={"Submit"} />
       </form>
-    </>
+     </div>
+    </div>
   );
 };
  
